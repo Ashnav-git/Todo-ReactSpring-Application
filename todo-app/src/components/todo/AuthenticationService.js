fb2479749d9fcf3e2780ef2b32ec60fc635e1531
Ashnav-git <69746930+Ashnav-git@users.noreply.github.com>
2021-07-17 12:51:23 +0530

    Basic Authentication

---
+++ todo-app/src/components/todo/AuthenticationService.js
@@ -0,0 +1,58 @@
+import axios from 'axios'
+class AuthenticationService {
+    executeBasicAuthenticationService(username,password)
+    {
+        
+        return axios.get(`http://localhost:8080/basicauth`,{headers : {authorization : this.createBasicAuthToken(username,password)}})
+    }
+    createBasicAuthToken(username,password)
+    {
+        return 'Basic '+ window.btoa(username + ":" +password)
+        
+    }
+    registerSuccesfullLogin(Username,password)
+    {
+        
+        sessionStorage.setItem('authenticatedUser',Username);
+        this.setUpAxiosInterceptors(this.createBasicAuthToken(Username,password))
+    }
+    logout()
+    {
+    sessionStorage.removeItem('authenticatedUser');
+    }
+
+    isUserLoggedIn()
+    {
+        let User  =  sessionStorage.getItem('authenticatedUser');
+        if(User === null) return false
+        return true
+        
+    }
+    getLoggedInUser()
+    {
+        let User  =  sessionStorage.getItem('authenticatedUser');
+        if(User === null) return ''
+        return User
+        
+    }
+
+    setUpAxiosInterceptors(basicAuthHeader)
+    {
+       
+        axios.interceptors.request.use(
+
+            config => {
+                if(this.isUserLoggedIn)
+                {
+                config.headers.authorization =basicAuthHeader
+                }
+                return config;
+            }
+        )
+       
+    }
+}
+
+
+
+export default new AuthenticationService()
